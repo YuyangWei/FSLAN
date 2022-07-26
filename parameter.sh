e90e216599c5c93e9b5129cf6de2c8e8802ea130
YuyangWei <69452414+YuyangWei@users.noreply.github.com>
2022-07-26 10:49:19 +0800

    Update parameter.sh

--- parameter.sh
+++ parameter.sh
@@ -1,8 +1,8 @@
-python main_gana.py --dataset FB15k-One --data_path ./FB15k --few 3 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 80 --early_stopping_patience 10 --dropout 0.1;
-python main_gana.py --dataset FB15k-One --data_path ./FB15k --few 3 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 60 --early_stopping_patience 10 --dropout 0.1;
-python main_gana.py --dataset FB15k-One --data_path ./FB15k --few 3 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 50 --early_stopping_patience 10 --dropout 0.1;
-python main_gana.py --dataset FB15k-One --data_path ./FB15k --few 3 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 110 --early_stopping_patience 10 --dropout 0.1;
-python main_gana.py --dataset FB15k-One --data_path ./FB15k --few 1 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 110 --early_stopping_patience 10 --dropout 0.1;
-python main_gana.py --dataset FB15k-One --data_path ./FB15k --few 2 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 110 --early_stopping_patience 10 --dropout 0.1;
-python main_gana.py --dataset FB15k-One --data_path ./FB15k --few 4 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 110 --early_stopping_patience 10 --dropout 0.1;
-python main_gana.py --dataset FB15k-One --data_path ./FB15k --few 5 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 110 --early_stopping_patience 10 --dropout 0.1;
+python main.py --dataset FB15k-One --data_path ./FB15k --few 3 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 80 --early_stopping_patience 10 --dropout 0.1;
+python main.py --dataset FB15k-One --data_path ./FB15k --few 3 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 60 --early_stopping_patience 10 --dropout 0.1;
+python main.py --dataset FB15k-One --data_path ./FB15k --few 3 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 50 --early_stopping_patience 10 --dropout 0.1;
+python main.py --dataset FB15k-One --data_path ./FB15k --few 3 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 110 --early_stopping_patience 10 --dropout 0.1;
+python main.py --dataset FB15k-One --data_path ./FB15k --few 1 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 110 --early_stopping_patience 10 --dropout 0.1;
+python main.py --dataset FB15k-One --data_path ./FB15k --few 2 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 110 --early_stopping_patience 10 --dropout 0.1;
+python main.py --dataset FB15k-One --data_path ./FB15k --few 4 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 110 --early_stopping_patience 10 --dropout 0.1;
+python main.py --dataset FB15k-One --data_path ./FB15k --few 5 --data_form Pre-Train --prefix fb15kone_gle3 --max_neighbor 110 --early_stopping_patience 10 --dropout 0.1;
